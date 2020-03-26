9e6c79e35062de4ff2f454aba8a5dc374e613e2f
Leandro Nogueira <40141700+Xanguana@users.noreply.github.com>
2020-03-26 12:23:05 -0300

    BackEnd Aplication

---
+++ backEnd/src/caderno.js
@@ -0,0 +1,32 @@
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Metodos HTTP
+ * 
+ * GET: buscar/listar informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parametros
+  * 
+  * Query Params: Paremetros nomeados enviados nas rotas apos "?"(filtros,paginações)
+  * Route Params: Paremetros utilizados para identificar recurso
+  * Request Body: Corpo da requisição, usado para criar ou alterar recurso
+  * 
+   */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft, SQL Server
+ * NoSQL: MongoDB, CouchDB, ...
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where(); 
+ */
