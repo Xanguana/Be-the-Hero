597eeb1c70daf660839e7bcf6289e7df09bd808f
Leandro Nogueira <40141700+Xanguana@users.noreply.github.com>
2020-04-01 18:32:15 -0300

    FrontEnd completada

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -7,21 +10,46 @@ import heroesImg from '../../assets/heroes.png';
 import logo from '../../assets/logo.svg'
 
 export default function Logon(){
+    const [id, setId] = useState( '' );
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try{
+            const response = await api.post('login', { id });
+            
+            localStorage.setItem('ongName', response.data.name);
+            localStorage.setItem('ongId', id);
+
+            history.push('./profile');
+
+        }catch (err){
+            alert("Usuario não autorizado");
+        }
+
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
                 <img src={logo} alt="Be the Hero"/>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange ={ e => setId(e.target.value)}
+                    />
+
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
-                        Não tenho cadastro.
-                    </a>
+                        Não tenho cadastro
+                    </Link>
                 </form>
             </section>
 
